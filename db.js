defe5df1b7b4d72e3390166e95c27fcb4ae88196
Isil Donmez <isil.donmez@gmail.com>
2018-07-20 17:30:49 +0100

    Set basics

--- db.js
+++ db.js
@@ -1,2 +1,5 @@
+const connectionUrl = process.env.MONGO_URL
+console.log(connectionUrl);
+
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere');
+mongoose.connect(connectionUrl, {});

e83ca7a313f548b3648b0c0cd64d3142707c4348
Isil Donmez <isil.donmez@gmail.com>
2018-07-20 17:58:41 +0100

    Add modules for JSON Web Tokens & encrypting passwords

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -4,3 +4,7 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 var User = require('../user/User');
+
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var config = require('../config');

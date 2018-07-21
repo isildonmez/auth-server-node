22cd93043855f204a005da8474ce5a33a15bd49c
Isil Donmez <isil.donmez@gmail.com>
2018-07-21 13:48:03 +0100

    Implement Logout

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -73,4 +73,14 @@ router.get('/me', function (req, res) {
   });
 });
 
+// Disclaimer: The logout endpoint is not needed.
+// The act of logging out can solely be done through the client side.
+// A token is usually kept in a cookie or the browser’s localstorage.
+// Logging out is as simple as destroying the token on the client.
+// This /logout endpoint is created to logically depict what happens when you log out.
+// The token gets set to null.
+router.get('/logout', function(req, res) {
+  res.status(200).send({ auth: false, token: null })
+});
+
 module.exports = router;
